1dda97cd54d0437b85f184ba07c2ded9da6643b6
EwennAnsquer <azbyansquer@gmail.com>
2024-03-11 14:48:42 +0100

    création du projet

---
+++ AP4/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AP4
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
